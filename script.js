7fd9ee695baa58a91683ddb3119a0ea31dcec020
Narek <nareka@10web.io>
2021-03-03 14:35:30 +0400

    changes on scrupt file

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-$.getJSON(
-  "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=c8edf9ab2ca118096215e9e36fbc7a20",
-  function (data) {
-    var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-    console.log(icon);
+function change() {
+    var city = $('#city-weather').val();
+    $('.city-input').val(city);
+    $.getJSON(
+      "http://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&APPID=c8edf9ab2ca118096215e9e36fbc7a20",
+      function (data) {
+          var icon = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+          var temp = Math.floor(data.main.temp);
+          var weather = data.weather[0].main;
+          $('.icon').prop('src', icon);
+          document.getElementById('temp1').innerHTML = temp;
+          document.getElementById('weather1').innerHTML = weather;
+      }
+    );
 }
-);
